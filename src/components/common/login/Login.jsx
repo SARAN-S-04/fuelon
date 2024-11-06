89d3da94ceaea284a060a2d2a403eb79eec2e178
mehan05 <mehanmehan6@gmail.com>
2024-11-06 15:27:20 +0530

    created login page

--- src/components/common/login/Login.jsx
+++ src/components/common/login/Login.jsx
@@ -5,75 +5,79 @@ const Login = () => {
                 <butron className=' pr-5 pl-5 pt-2 pb-2  rounded-lg bg-[#5072f5] border-2 '>Signup</butron>
         </div>
 
-        <div className="flex justify-around absolute top-[142px] left-[171px]">
-            <div className="flex flex-col gap-6">
-                  <h1 className="text-4xl text-[#161439] font-semibold font-poppins">Welcome!</h1>
-                  <h3 className="text-3xl text-[#161439] font-semibold font-poppins ">Login to your account</h3>
-                  <div className="w-[399.32px ] h-[266px]">
-                      <img src="/loginPageImage.png" alt="Image" />
-                  </div>
-            </div>
+        <div className="flex justify-around relative">
+          <div className="absolute top-[10px] left-[20px]">
 
-            <div>
-                        <form className="max-w-sm mx-auto">
-                  <div className="mb-5">
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                      Your email
+              <div className="flex flex-col gap-6">
+                    <h1 className="text-4xl text-[#161439] font-semibold font-poppins">Welcome!</h1>
+                    <h3 className="text-3xl text-[#161439] font-semibold font-poppins ">Login to your account</h3>
+                    <div className="w-[399.32px ] h-[266px]"> 
+                        <img src="/loginPageImage.png" alt="Image" />
+                    </div>
+              </div>
+          </div>
+
+          <div className="absolute top-[20px] left-[759px] border-2 p-10 w-[508px] h-[512px] rounded-lg">
+ 
+              <div className="max-w-sm mx-auto " >
+                <form >
+                  <div className="mb-5 flex flex-col items-start"  >
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:black ">
+                      Email
                     </label>
                     <input
                       type="email"
                       id="email"
-                      className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                      placeholder="name@flowbite.com"
+                      className="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:black dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                      placeholder="sahil@gmail.com"
                       required
                     />
                   </div>
-                  <div className="mb-5">
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                      Your password
+                  <div className="mb-5 flex flex-col items-start">
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:black">
+                      Password
                     </label>
                     <input
                       type="password"
                       id="password"
-                      className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                      className="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:black dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                       required
                     />
                   </div>
-                  <div className="mb-5">
-                    <label htmlFor="repeat-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                      Repeat password
+                  <div className="mb-5 flex flex-col items-start">
+                    <label htmlFor="repeat-password" className="block mb-2 text-sm font-medium text-gray-900 dark:black">
+                      Role
                     </label>
-                    <input
-                      type="password"
-                      id="repeat-password"
-                      className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                      required
-                    />
+                    <select name="role" id=""  className="shadow-sm  border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:black dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                      required>
+                      <option value="trainee">Trainee</option>
+                      <option value="Student">Student</option>
+                    </select>
+                   
                   </div>
-                  <div className="flex items-start mb-5">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="terms"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                        required
-                      />
-                    </div>
-                    <label htmlFor="terms" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                      I agree with the{' '}
-                      <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">
-                        terms and conditions
-                      </a>
-                    </label>
+                  <div className="flex justify-between mb-5">
+                     <div>
+                        <button className="text-gray-500">Forgot Password?</button >
+                     </div>
+                     <div>
+                        <button className="rounded-lg pr-5 pl-5 pt-2 pb-2 bg-[#5072f5] border-2 ">Login</button >
+                     </div>
                   </div>
-                  <button
-                    type="submit"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Register new account
-                  </button>
+                      <div className=" flex justify-between items-center" >
+                            <p>Login With</p>
+                            <div className="flex gap-6">
+                              <img src="/facebookLogo.png" alt="fcebookLogo"   className="w-[42px] h-[42px]"/>
+                              <img src="/GoogleLogo.png" alt="GoogleLogo" className="w-[42px] h-[42px]"/>
+                              <img src="/linkedinLogo.png" alt="LinkedinLogo" className="w-[42px] h-[42px]"/>
+                            </div>
+
+                      </div>
                 </form>
-            </div>
+              </div>
+          </div>
+
+
+            
         </div>
     </div>
   )
