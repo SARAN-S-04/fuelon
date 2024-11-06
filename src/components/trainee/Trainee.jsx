49f9a2b434e3b8b13c5b176391419a488a99a6db
mehan05 <mehanmehan6@gmail.com>
2024-11-06 18:52:56 +0530

    added the mycourse page

--- src/components/trainee/Trainee.jsx
+++ src/components/trainee/Trainee.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Login from '../common/login/Login'
-
+import MyCourses from './MyCourses/MyCourses'
 const Trainee = () => {
   return (
 
     <div>
     <Routes>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/trainne/mycourses' element={<MyCourses/>}/>
     </Routes>
           
     </div>
