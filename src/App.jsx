13b439674c50df0ccaaf2a232ae911ee021b5494
SARAN-S-04 <sarans0504@gmail.com>
2024-11-07 23:35:55 +0530

    tet

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css'
 import Login from './components/common/login/Login'
 import Trainee from './components/trainee/Trainee'
-
+import Navbar from  './components/common/navbar/navbar'
+import Landing from './components/common/landing/landing'
 function App() {
  
   return (
     <>
-     <Trainee/>
+     <Landing/>
+     
     </>
   )
 }
---
+++ src/components/common/landing/landing.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from '../navbar/navbar';
+
+function Landing() {
+  return (
+    <div>
+      <Navbar />
+
+      <div className="w-full bg-gray-200 px-4 py-20 flex flex-col md:flex-row items-center justify-between">
+        <div className="md:w-1/2">  
+          <h1 className="text-5xl font-bold text-gray-800">Learn Without <span className="bg-yellow-500 text-black px-2">Limits</span></h1>
+          <p className="text-lg text-gray-500 mt-4">Every teaching and learning journey is unique.</p> 
+          <h2 className="text-2xl font-bold text-gray-700 mt-4">With platformname</h2>
+          <p className='text-lg text-gray-500 mt-4'>We'll help guide your way.</p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4">Start Free Trial</button>
+        </div>
+
+        <div className="md:w-1/4">
+          <img src="/landingimage.png" alt="Learn Without Limits" className="object-cover w-25 h-10 md:h-auto" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
