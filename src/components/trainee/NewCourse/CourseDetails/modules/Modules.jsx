362a4385367a9f8777499bc035603a008fa82486
SARAN-S-04 <sarans0504@gmail.com>
2024-11-07 23:45:18 +0530

    test

--- src/components/trainee/NewCourse/CourseDetails/modules/Modules.jsx
+++ src/components/trainee/NewCourse/CourseDetails/modules/Modules.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import ModuleForm from './components/ModuleForm'
-
+import ModuleForm from '../Level/components/ModuleForm'
 
 const Modules = () => {
 
   return (
-    <div className='flex flex-col items-center'>
+    <div>
         <div className="flex flex-col items-start mb-4">
             <h1 className="text-3xl md:text-4xl font-bold mb-1">Modules</h1>
             <span className="text-gray-400">Add Your Modules here...</span>
@@ -20,10 +19,7 @@ const Modules = () => {
           <button className="bg-gray-300 hover:bg-violet-500  font-bold py-2 px-4 rounded-md text-black text-xl"><img src="/plusicon.png" className='w-5 h-5' alt="" /></button>
         </div>
       </div>
-        <div className='w-full max-w-4xl'>
-
-            <ModuleForm/>
-        </div>
+      <ModuleForm/>
     </div>
   )
 }
